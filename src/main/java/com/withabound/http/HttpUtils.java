388b8ea0d21678d1440b18327f40da714c93fce9
Mark Mullan <mark.mullan@clover.com>
2021-11-11 23:26:32 -0800

    appendQueryParams doc

--- src/main/java/com/withabound/http/HttpUtils.java
+++ src/main/java/com/withabound/http/HttpUtils.java
@@ -6,6 +6,13 @@ import okhttp3.HttpUrl;
 
 public class HttpUtils {
   /**
+   * A noteworthy limitation of this method is that it cannot support multiple query parameters with
+   * the same key. For example, the styles ?list=red,blue,green and ?list=red&list=blue&list=green
+   * are both valid (https://datatracker.ietf.org/doc/html/rfc6570#section-3.2.8) and strive to
+   * convey the same meaning, but this method will currently not handle either of those strategies.
+   * At time of writing, Abound's APIs do not support logical OR filtering of the same query
+   * parameter key, so this feature is not yet required.
+   *
    * @param url without query params
    * @param params POJO that will be converted to query params
    * @return a url decorated with the query params

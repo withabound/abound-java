30fe6f784306fed825d032e60036fcba64b251d0
Mark Mullan <markrmullan@gmail.com>
2021-12-14 08:04:46 -0800

    FEAT: add 1099-NEC document request

---
+++ src/main/java/com/withabound/models/documents/ten99nec/Form1099NECDocumentRequest.java
@@ -0,0 +1,61 @@
+package com.withabound.models.documents.ten99nec;
+
+import com.withabound.models.documents.DocumentRequest;
+import com.withabound.models.documents.DocumentType;
+import com.withabound.models.documents.Ten99MISCAndTen99NECStateTaxInfo;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import lombok.Builder;
+import lombok.Setter;
+
+@Builder
+@Setter
+public class Form1099NECDocumentRequest extends DocumentRequest {
+  private String payerId;
+
+  private int year;
+
+  private String accountNumber;
+
+  private double nonemployeeCompensation;
+
+  private Boolean hasDirectSalesOver5000;
+
+  private Double federalIncomeTaxWithheld;
+
+  private List<Ten99MISCAndTen99NECStateTaxInfo> stateTaxInfo;
+
+  @Override
+  public DocumentType getType() {
+    return DocumentType.TEN99NEC;
+  }
+
+  public String getPayerId() {
+    return payerId;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public Optional<String> getAccountNumber() {
+    return Optional.ofNullable(accountNumber);
+  }
+
+  public double getNonemployeeCompensation() {
+    return nonemployeeCompensation;
+  }
+
+  public Boolean getHasDirectSalesOver5000() {
+    return hasDirectSalesOver5000;
+  }
+
+  public Double getFederalIncomeTaxWithheld() {
+    return federalIncomeTaxWithheld;
+  }
+
+  public List<Ten99MISCAndTen99NECStateTaxInfo> getStateTaxInfo() {
+    return stateTaxInfo == null ? Collections.emptyList() : stateTaxInfo;
+  }
+}

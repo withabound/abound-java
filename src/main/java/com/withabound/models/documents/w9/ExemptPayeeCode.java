2b7e8da609c14c42d14ed0189c274b1ff2c93c5f
Brent Mealhouse <bmealhouse@gmail.com>
2022-11-22 11:47:49 -0600

    Add exemptPayeeCode and exemptFatcaCode to Form W-9 (#65)

---
+++ src/main/java/com/withabound/models/documents/w9/ExemptPayeeCode.java
@@ -0,0 +1,44 @@
+package com.withabound.models.documents.w9;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum ExemptPayeeCode {
+  @SerializedName("1")
+  ONE,
+
+  @SerializedName("2")
+  TWO,
+
+  @SerializedName("3")
+  THREE,
+
+  @SerializedName("4")
+  FOUR,
+
+  @SerializedName("5")
+  FIVE,
+
+  @SerializedName("6")
+  SIX,
+
+  @SerializedName("7")
+  SEVEN,
+
+  @SerializedName("8")
+  EIGHT,
+
+  @SerializedName("9")
+  NINE,
+
+  @SerializedName("10")
+  TEN,
+
+  @SerializedName("11")
+  ELEVEN,
+
+  @SerializedName("12")
+  TWELVE,
+
+  @SerializedName("13")
+  THIRTEEN,
+}

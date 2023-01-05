f42c944f36b249c340c414c9f61ede5699f5613d
Brent Mealhouse <bmealhouse@gmail.com>
2023-01-05 10:36:20 -0600

    Update W9 types (#69)

--- src/main/java/com/withabound/models/documents/w9/W9TaxClassification.java
+++ src/main/java/com/withabound/models/documents/w9/W9TaxClassification.java
@@ -3,6 +3,36 @@ package com.withabound.models.documents.w9;
 import com.google.gson.annotations.SerializedName;
 
 public enum W9TaxClassification {
+  @SerializedName("individual")
+  INDIVIDUAL,
+
   @SerializedName("soleProprietor")
   SOLE_PROPRIETOR,
+
+  @SerializedName("singleMemberLlc")
+  SINGLE_MEMBER_LLC,
+
+  @SerializedName("cCorporation")
+  C_CORPORATION,
+
+  @SerializedName("sCorporation")
+  S_CORPORATION,
+
+  @SerializedName("partnership")
+  PARTNERSHIP,
+
+  @SerializedName("trust")
+  TRUST,
+
+  @SerializedName("estate")
+  ESTATE,
+
+  @SerializedName("llcCCorporation")
+  LLC_C_CORPORATION,
+
+  @SerializedName("llcSCorporation")
+  LLC_S_CORPORATION,
+
+  @SerializedName("llcPartnership")
+  LLC_PARTNERSHIP,
 }
